337cbcc572f5b5ea7b96c492821b64949fe0aac9
random-anon33 <contact@vonsteinkirch.com>
2025-12-04 20:02:24 +0800

    todo

--- scripts/applications/todo/todo-storage.js
+++ scripts/applications/todo/todo-storage.js
@@ -15,25 +15,7 @@ class TodoStorage {
 
     getTodos() {
         const defaultTodoData = [
-            { text: 'organize the research panel with sophia\'s data', completed: false },
-            { text: 'work on the characters of our heroes', completed: false },
-            { text: 'work on the crime scene', completed: false },
-            { text: 'write to kolya', completed: false },
-            { text: 'envisage the first act unfolding', completed: false },
-            { text: 'get a new agent', completed: false },
-            { text: 'open source the node', completed: true },
-            { text: 'meet sophia', completed: true },
-            { text: 'research about creepy.xyz', completed: true },
-            { text: 'dive into the mind of the first antagonist', completed: true },
-            { text: 'play with gemini 3', completed: true },
-            { text: 'install omarchy', completed: true },
-            { text: 'figure out whats that banana thing', completed: true },
-            { text: 'order some orange merch and a virtuix', completed: true },
-            { text: 'find a recipe for iced milo', completed: true },
-            { text: 'ask the robot what drop the top mean', completed: true },
-            { text: 'watch jedi search', completed: true },
-            { text: 'get the dw+ blackfriday deal', completed: true },
-            { text: 'buy the dip', completed: true }
+            { text: 'get ready for the best year ever yet', completed: false }
         ];
 
         return defaultTodoData.map(data => this.createTodo(data.text, data.completed));
